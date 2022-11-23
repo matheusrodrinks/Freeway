c60d6b6599d605899784fd64f0915e048e3b9802
MATHEUS RODRIGUES MENDONÇA <82005382+matheusrodrinks@users.noreply.github.com>
2022-11-23 11:22:54 -0400

    changes

--- ator.js
+++ ator.js
@@ -1,6 +1,9 @@
 //código do ator
+
 let xAtor = 100;
 let yAtor = 366;
+let colisao = false;
+
 
 function mostraAtor(){
   image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -13,4 +16,18 @@ function movimentaAtor(){
   if (keyIsDown(DOWN_ARROW)){
     yAtor += 3;
   }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+      colidiu();
+    }
+  }
+}
+
+function colidiu(){
+  yAtor = 366;
 }
